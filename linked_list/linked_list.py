fd78c00079334d32b88ca413d848bfd6d0f72140
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-03-24 10:32:17 +0000

    LL print list

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -30,11 +30,23 @@ class Linked_list():
         self.length+=1
         return self
 
+    def printList(self):
+        list_values = []
+        currentNode = self.head
+
+        while (currentNode!=None):
+            list_values.append(currentNode['data'])
+            currentNode = currentNode['next']
+        return list_values
+
+    def insert(self, index, value):
+        pass
+        for i in range(index):
+            pass
+            
 
 linked_list = Linked_list(5)
 print(linked_list.head['data'])
 print(linked_list.append(10))
 print(linked_list.prepend(20).head)
-
-
-    
+print(linked_list.printList())
